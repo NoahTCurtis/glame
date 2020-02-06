b9ce24201dafb4e77a01957884cbfceeef47dbd1
Noah Curtis <noahtcurtis@gmail.com>
2020-02-05 23:47:12 -0800

    Added intro scene. Finalized test scene for demo.

---
+++ Assets/Scenes/Splash/FlashingPages.cs
@@ -0,0 +1,26 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class FlashingPages : MonoBehaviour
+{
+	public List<Sprite> Pages;
+
+	float timePerPage = 0.1f; //in seconds
+	float timeSoFar = 0;
+
+	void Update()
+	{
+		timeSoFar += Time.deltaTime;
+
+		if(timeSoFar >= timePerPage)
+		{
+			timeSoFar -= timePerPage;
+
+			//pick a new page
+			int i = Random.Range(0, Pages.Count);
+			GetComponent<Image>().sprite = Pages[i];
+		}
+	}
+}
---
+++ Assets/Scenes/Splash/SplashControl.cs
@@ -0,0 +1,65 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+using UnityEngine.SceneManagement;
+
+public class SplashControl : MonoBehaviour
+{
+	public GameObject toha;
+	public GameObject vdo;
+	public GameObject blame;
+	public GameObject text1;
+	public GameObject text2;
+
+	public Image FillBar;
+	private float charge = 0;
+
+	void Start()
+	{
+		toha.SetActive(true);
+		vdo.SetActive(true);
+		blame.SetActive(true);
+		text1.SetActive(true);
+		text2.SetActive(true);
+		StartCoroutine(introAnim());
+	}
+	
+	IEnumerator introAnim()
+	{
+		yield return new WaitForSeconds(8.0f);
+		toha.SetActive(false);
+		yield return new WaitForSeconds(4.5f);
+		vdo.SetActive(false);
+		yield return new WaitForSeconds(7);
+		blame.SetActive(false);
+
+		while (Input.anyKeyDown == false) yield return null;
+		text1.SetActive(false);
+
+		while (true)
+		{
+			Debug.Log("waiting for charge");
+			float secondsItTakesToCharge = 1.75f;
+			float secondsItTakesToUncharge = 0.75f;
+			if (Input.GetMouseButton(1))
+			{
+				charge += Time.deltaTime / secondsItTakesToCharge;
+			}
+			else
+			{
+				charge -= Time.deltaTime / secondsItTakesToUncharge;
+			}
+			charge = Mathf.Clamp(charge, 0, 1);
+
+			FillBar.fillAmount = charge;
+
+			if (Input.GetMouseButtonDown(0) && charge > 0.99f)
+				break;
+
+			yield return null;
+		}
+
+		SceneManager.LoadScene("TestScenePlsIgnore", LoadSceneMode.Single);
+	}
+}
