b8a6776d9b8289516efff7fb0638cac84fcd47b4
Noah Curtis <noahtcurtis@gmail.com>
2020-04-20 23:51:16 -0700

    Added animation to exterminator rig and made a simple ai in the darkness level

---
+++ Assets/NavMeshAgentGotoObject.cs
@@ -0,0 +1,22 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.AI;
+
+[RequireComponent(typeof(NavMeshAgent))]
+public class NavMeshAgentGotoObject : MonoBehaviour
+{
+	public Transform TargetTransform;
+
+	private NavMeshAgent _agent;
+
+	void Start()
+	{
+		_agent = GetComponent<NavMeshAgent>();
+	}
+	
+	void Update()
+	{
+		_agent.SetDestination(TargetTransform.position);
+	}
+}
